81730c645f2e7082cd5e35a02fc606e52dec14c9
Siva Mahadevan <svmhdvn@fastmail.com>
2020-01-04 22:57:06 -0500

    converting dotfiles repo into $HOME

---
+++ .emacs.d/custom-settings.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (mu4e which-key visual-fill-column projectile magit ledger-mode swiper counsel ivy ido-vertical-mode exec-path-from-shell elfeed editorconfig))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
