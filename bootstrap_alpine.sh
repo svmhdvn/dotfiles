61f4a2c138ba73c86cec5759a8ce8ca623c751ee
Siva Mahadevan <me@svmhdvn.name>
2024-01-29 20:08:24 +0000

    more alpine bootstrap fixes

--- bootstrap_alpine.sh
+++ bootstrap_alpine.sh
@@ -21,11 +21,15 @@ doas apk add
 
 doas rm -rf /etc/iwd /etc/acpi/LID
 doas ln -sf "$host_config"/etc/acpi/LID /etc/acpi/LID
-doas ln -sf "$host_config"/etc/iwd /etc/iwd
 
-ssh-keygen -t ed25519
-eval "$(ssh-agent -s)"
-ssh-add "$HOME/.ssh/id_ed25519"
+doas ln -sf "$host_config"/etc/iwd /etc/iwd
+doas rc-update add iwd
+
+if test ! -r "$HOME/.ssh/id_ed25519.pub"; then
+	ssh-keygen -t ed25519
+	eval "$(ssh-agent -s)"
+	ssh-add "$HOME/.ssh/id_ed25519"
+fi
 
 # sway setup
 doas setup-devd udev
@@ -33,27 +37,38 @@ doas adduser "$(whoami)" audio
 doas adduser "$(whoami)" video
 doas adduser "$(whoami)" input
 doas adduser "$(whoami)" seat
+doas rc-update add seatd
 
 # alpine ports setup
 doas adduser "$(whoami)" abuild
 doas mkdir -p /var/cache/distfiles
 doas chgrp abuild /var/cache/distfiles
 doas chmod g+w /var/cache/distfiles
+
+# TODO make this idempotent
 doas abuild-keygen -a -i
 
 echo "SYNCTHING_USER=$(whoami)" | doas tee /etc/conf.d/syncthing
+doas rc-update add syncthing
 
 # TODO change hardcoded kernel config name
-KERNEL_CONFIG="$PWD/kernel_configs/thinkpad-e495_current.config"
-LATEST_LINUX_KERNEL=$(sed -n -e '4q' -e 's/.* \([0-9]*\.[0-9]*\.[0-9]*\) .*/\1/p' "$KERNEL_CONFIG")
-git clone --depth 1 --branch "v$LATEST_LINUX_KERNEL" "$HOME/src/linux-stable"
-cp "$KERNEL_CONFIG" "$HOME/src/linux-stable"
 
-git clone https://github.com/swaywm/sway "$HOME/src/sway"
-./bin/mybuild-sway
+if test ! -d "$HOME/src/linux-stable"; then
+	KERNEL_CONFIG="$PWD/kernel_configs/thinkpad-e495_current.config"
+	LATEST_LINUX_KERNEL=$(sed -n -e '4q' -e 's/.* \([0-9]*\.[0-9]*\.[0-9]*\) .*/\1/p' "$KERNEL_CONFIG")
+	git clone --depth 1 --branch "v$LATEST_LINUX_KERNEL" git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git "$HOME/src/linux-stable"
+	cp "$KERNEL_CONFIG" "$HOME/src/linux-stable"
+fi
 
-git clone https://github.com/mawwww/kakoune "$HOME/src/kakoune"
-./bin/mybuild-kakoune
+if test ! -d "$HOME/src/sway"; then
+	git clone https://github.com/swaywm/sway "$HOME/src/sway"
+	./bin/mybuild-sway
+fi
+
+if test ! -d "$HOME/src/kakoune"; then
+	git clone https://github.com/mawww/kakoune "$HOME/src/kakoune"
+	./bin/mybuild-kakoune
+fi
 
 mbsync -a
 
