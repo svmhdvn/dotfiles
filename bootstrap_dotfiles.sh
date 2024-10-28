c3ad778c9dec7554aafe128ad5247942d681157f
Siva Mahadevan <me@svmhdvn.name>
2024-10-27 22:42:45 -0400

    lol nixos not great, alpine GOAT

--- bootstrap_dotfiles.sh
+++ bootstrap_dotfiles.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
-set -ex
+set -eux
 
-common_config_prefix="$HOME/dotfiles/common_config"
-host_specific_prefix="$HOME/dotfiles/host_specific/$(hostname)/config"
+dotfiles="${HOME}/s/dotfiles"
+ln -sf "${dotfiles}/bin" "${HOME}/bin"
 
-mkdir -p "$HOME/.config" "$HOME/.local/bin" "$HOME/src"
+common_config_prefix="${dotfiles}/common_config"
+host_specific_prefix="${dotfiles}/host_specific/$(hostname)/config"
 
-cd "$common_config_prefix"
+ln -sf "${host_specific_prefix}/profile" "${HOME}/.profile"
+
+mkdir -p "${HOME}/.config"
+cd "${common_config_prefix}"
 for f in *; do
-	if [ -e "$host_specific_prefix/$f" ]; then
-		ln -sf "$host_specific_prefix/$f" "$HOME/.config/$f"
+	if test -e "${host_specific_prefix}/${f}" ; then
+		ln -sf "${host_specific_prefix}/${f}" "${HOME}/.config/${f}"
 	else
-		ln -sf "$common_config_prefix/$f" "$HOME/.config/$f"
+		ln -sf "${common_config_prefix}/${f}" "${HOME}/.config/${f}"
 	fi
 done
-
-ln -sf "$HOME/dotfiles/bin" "$HOME/bin"
-ln -sf "$host_specific_prefix/profile" "$HOME/.profile"
-
-# TODO move mbsyncrc to XDG_CONFIG_HOME once supported upstream
-ln -sf "$common_config_prefix/mbsyncrc" "$HOME/.mbsyncrc"
