0246d5adab51975776e0bd025b32792cb555448c
Siva Mahadevan <me@svmhdvn.name>
2025-11-06 17:39:02 -0500

    bak

--- bootstrap_dotfiles.sh
+++ bootstrap_dotfiles.sh
@@ -5,7 +5,7 @@ dotfiles="${HOME}/src/dotfiles"
 ln -sf "${dotfiles}/bin" "${HOME}/bin"
 
 common_config_prefix="${dotfiles}/common_config"
-host_specific_prefix="${dotfiles}/host_specific/$(hostname)/config"
+host_specific_prefix="${dotfiles}/host_specific/$(hostname)"
 
 if test -e "${host_specific_prefix}/profile"; then
   ln -sf "${host_specific_prefix}/profile" "${HOME}/.profile"
@@ -15,11 +15,11 @@ mkdir -p "${HOME}/.config"
 
 cd "${common_config_prefix}"
 for f in *; do
-  ln -sf "${common_config_prefix}/${f}" "${HOME}/.config/${f}"
+  ln -nsf "${common_config_prefix}/${f}" "${HOME}/.config/${f}"
 done
 
 # override with host-specific config after
-cd "${host_specific_prefix}"
+cd "${host_specific_prefix}/config"
 for f in *; do
-  ln -sf "${host_specific_prefix}/${f}" "${HOME}/.config/${f}"
+  ln -nsf "${host_specific_prefix}/config/${f}" "${HOME}/.config/${f}"
 done
