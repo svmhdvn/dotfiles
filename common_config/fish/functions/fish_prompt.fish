81fd2ae7fcb8f748be0ff1b24878371cd3309ba6
Siva Mahadevan <me@svmhdvn.name>
2025-12-20 12:01:45 -0500

    bak

--- common_config/fish/functions/fish_prompt.fish
+++ common_config/fish/functions/fish_prompt.fish
@@ -1,7 +1,7 @@
 function fish_prompt
     if set -q ZMX_SESSION
         set_color $fish_color_host_remote
-        echo -n "[$hostname.$ZMX_SESSION] "
+        echo -n "[$ZMX_SESSION] "
     end
     set_color $fish_color_cwd
     echo -n (basename $PWD)') '
